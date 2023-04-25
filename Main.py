e743985d111077b743ccae0d544297b8e4386154
HenryofSkalitz1202 <110165919+HenryofSkalitz1202@users.noreply.github.com>
2023-04-25 20:12:30 +0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,11 @@
+from instruction import instruction_umum
+from load import load
+import time
+
+print("Loading...")
+time.sleep(2)
+load()
+print('==================================================================')
+print('         RORO JONGGRANG, LEGENDA DI BALIK CANDI PRAMBANAN         ')
+print('==================================================================')
+instruction_umum()
