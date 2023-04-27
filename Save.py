dca46ce8ee744e8dcf2908e9b75a1795c266ea5b
HenryofSkalitz1202 <110165919+HenryofSkalitz1202@users.noreply.github.com>
2023-04-27 22:30:27 +0700

    Update Save.py

--- Save.py
+++ Save.py
@@ -22,10 +22,6 @@ def save(arr_user, arr_candi, arr_bahan):
     path_candi = f'{path}/candi.csv'
     path_bahan = f'{path}/bahan_bangunan.csv'
 
-    print(arr_user)
-    print(arr_candi)
-    print(arr_bahan)
-
     write_user(path_user, arr_user)
     write_candi(path_candi, arr_candi)
     write_bahan(path_bahan, arr_bahan)
