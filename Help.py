4ce8407ed06da3d227573fef4199702f870679b1
HenryofSkalitz1202 <110165919+HenryofSkalitz1202@users.noreply.github.com>
2023-04-30 23:07:53 +0700

    Update Help.py

--- Help.py
+++ Help.py
@@ -6,30 +6,48 @@ def help_umum():
     print("=========== HELP ===========")
     print("1. login")
     print("   Untuk masuk menggunakan akun")
-    print("2. exit")
-    print("   Untuk keluar dari program dan kembali ke terminal")
+    print("2. save")
+    print("   Menyimpan data program ke folder save")
+    print("3. exit")
+    print("   Keluar dari program dan kembali ke terminal")
 
 
 def help_bondowoso():
     print("=========== HELP ===========")
-    print("1. Logout")
-    print("   Untuk keluar dari akun yang digunakan sekarang")
-    print("2. Summon jin")
-    print("   Untuk memanggil jin dari alam gaib")
-    print("3. Hilangkan jin ")
-    print("   untuk menghapus jin dari alam gaib")
-    print("4. Ubah Jin")
-    print("   Mengubah tipe jin yang telah disummon dari alam gaib")
+    print("1. logout")
+    print("   Keluar dari akun yang digunakan sekarang")
+    print("2. summonjin")
+    print("   Memanggil jin dari alam gaib")
+    print("3. hilangkanjin ")
+    print("   Menghapus jin dari alam gaib")
+    print("4. ubahjin")
+    print("   Mengubah tipe jin yang telah di-summon dari alam gaib")
+    print("5. batchkumpul")
+    print("   Seluruh jin dengan tipe pengumpul akan mengumpulkan bahan")
+    print("6. batchbangun")
+    print("   Seluruh jin dengan tipe pembangun akan membangun candi")
+    print("7. laporanjin")
+    print("   Menampilkan jumlah jin, jin terajin & termalas, dan jumlah bahan")
+    print("8. laporancandi")
+    print("   Menampilkan jumlah candi, jumlah bahan candi, dan ID candi termurah & termahal")
+    print("9. save")
+    print("   Menyimpan data program ke folder save")
+    print("10. exit")
+    print("   Keluar dari program dan kembali ke terminal")
 
 
 def help_roro():
     print("=========== HELP ===========")
     print("1. logout")
     print("   Untuk keluar dari akun yang digunakan sekarang")
-    print("2. Hancurkan candi")
+    print("2. hancurkancandi")
     print("   Untuk menghancurkan candi yang tersedia")
-    print("3. Aktifkan Ayam berkokok")
+    print("3. ayamberkokok")
     print("   Roro mempunyai kemampuan untuk memalsukan pagi hari ")
+    print("4. save")
+    print("   Menyimpan data program ke folder save")
+    print("5. exit")
+    print("   Keluar dari program dan kembali ke terminal")
 
 
 def help_pengumpul():
@@ -38,6 +56,10 @@ def help_pengumpul():
     print("   Untuk keluar dari akun yang digunakan sekarang")
     print("2. kumpul")
     print("   Untuk mengumpulkan resource candi")
+    print("3. save")
+    print("   Menyimpan data program ke folder save")
+    print("4. exit")
+    print("   Keluar dari program dan kembali ke terminal")
 
 
 def help_pembangun():
@@ -46,6 +68,10 @@ def help_pembangun():
     print("   Untuk keluar dari akun yang digunakan sekarang")
     print("2. bangun")
     print("   Untuk membangun candi")
+    print("3. save")
+    print("   Menyimpan data program ke folder save")
+    print("4. exit")
+    print("   Keluar dari program dan kembali ke terminal")
 
 
 def list_help(role):
